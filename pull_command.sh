a494367971da2b53c0d4fe213c99bdc776929e7e
other-ones <tuk101234@gmail.com>
2024-07-19 12:01:39 -0400

    update

--- pull_command.sh
+++ pull_command.sh
@@ -1,1 +1,3 @@
 git pull origin main
+cp assets/modeling_clip.py /home/twkim/anaconda3/envs/char/lib/python3.9/site-packages/transformers/models/clip/modeling_clip.py
+cp assets/modeling_outputs.py /home/twkim/anaconda3/envs/char/lib/python3.9/site-packages/transformers/transformers/modeling_outputs.py
